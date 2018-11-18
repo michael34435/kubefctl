ff232b6a3dbc8bf5216e4b408f4831c51b34a389
Michael.K <michael34435@capslock.tw>
2018-11-18 19:30:47 +0800

    feat: remove ingress when deleting clusters

--- src/commands/delete/credential.js
+++ src/commands/delete/credential.js
@@ -1,13 +1,17 @@
 const fs = require('fs-extra');
 const exec = require('../../libs/exec');
+const _ = require('lodash');
 
 exports.command = 'credential <CLUSTER_NAME>';
 exports.description = 'Delete stored federation clusters on GKE.';
 exports.handler = async (command) => {
   const clusterName = command.CLUSTER_NAME;
   const kubeConf = `${process.env.HOME}/.kubefctl/clusters/${clusterName}.yml`;
+  const kubeDeploymentConf = `${process.env.HOME}/.kubefctl/tmp/${clusterName}.ingress.yml`;
+  const kubeIngressConf = `${process.env.HOME}/.kubefctl/tmp/${clusterName}.ingress.yml`;
   const kubeNamesConf = `${process.env.HOME}/.kubefctl/cluster-name/${clusterName}.json`;
   const kubeList = `${process.env.HOME}/.kubefctl/list`;
+  const kubemciIngressConf = `${process.env.HOME}/.kubefctl/ingress`;
 
   if (!fs.existsSync(kubeConf)) {
     throw new Error(`Error: federation/clusters "${clusterName}" not found`);
@@ -26,11 +30,23 @@ exports.handler = async (command) => {
   }
 
   const federations = fs.readJsonSync(kubeList, { throws: false });
-  federations.splice(federations.findIndex(federation => federation.clusterName === clusterName), 1);
+  federations.splice(federations.findIndex((federation) => federation.clusterName === clusterName), 1);
+
+  // delete ingress
+  const ingresses = _.defaultTo(fs.readJsonSync(kubemciIngressConf), {});
+
+  if (ingresses[clusterName]) {
+    await exec(`kubemci delete ${ingresses[clusterName]} --ingress=${kubeIngressConf} --kubeconf=${kubeConf}`);
+
+    delete ingresses[clusterName];
+    fs.writeJsonSync(kubemciIngressConf, ingresses);
+  }
 
   fs.ensureFileSync(kubeNamesConf);
   fs.unlinkSync(kubeConf);
   fs.unlinkSync(kubeNamesConf);
+  fs.unlinkSync(kubeDeploymentConf);
+  fs.unlinkSync(kubeIngressConf);
   fs.writeJsonSync(kubeList, federations);
 
   console.log(`federation/clusters "${clusterName}" deleted`);
